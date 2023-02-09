d86474e69ca2a3c56eaf229854931c3957d8ed75
subin9804 <subin9804@naver.com>
2023-02-10 05:46:01 +0900

    edit4

--- public-data-app/src/App.js
+++ public-data-app/src/App.js
@@ -1,8 +1,12 @@
-import React, {useState, useContext, useEffect, useRef} from 'react';
+import React, {useState, useContext, useEffect, useRef, PureComponent} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Map, MapMarker, MapInfoWindow} from 'react-kakao-maps-sdk';
 import {
+  Cell,
+  Sector,
+  PieChart,
+  Pie,
   ComposedChart,
   Bar,
   XAxis,
@@ -10,97 +14,83 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer
 } from "recharts";
 
 
+function fetchData(searchYearCd, guGun) {
 
-const seoulGuguns = {
-  강남구: 1116,
-  강동구: 1117,
-  강북구: 1124, 
-  강서구: 1111, 
-  관악구: 1115,
-  광진구: 1123,
-  구로구: 1112,
-  금천구: 1125,
-  노원구: 1122,
-  도봉구: 1107,
-  동대문구: 1105,
-  동작구: 1114, 
-  마포구: 1110,
-  서대문구: 1109,
-  서초구: 1119,
-  성동구: 1104,
-  성북구: 1106,
-  송파구: 1118,
-  양천구: 1120,
-  영등포구: 1113,
-  용산구: 1103,
-  은평구: 1108, 
-  종로구: 1101, 
-  중구: 1102,
-  중랑구: 1121
-}
-
-const GUGUN = object.keys(seoulGuguns);
-
-function fetchData(searchYearCd, Guguns) {
   const endPoint = 'http://apis.data.go.kr/B552061/lgStat/getRestLgStat'
   const serviceKey = process.env.REACT_APP_SERVICE_KEY;
   const type = 'json';
-  const numOfRows = 13
-  const siDo = 1100
-  const pageNo = 1
+  const numOfRows = 13;
+  const siDo = 1100;
+  const pageNo = 1;
 
+  const promise = fetch(`${endPoint}?serviceKey=${serviceKey}&searchYearCd=${searchYearCd}&siDo=${siDo}&guGun=${guGun}&type=${type}&numOfRows=${numOfRows}&pageNo=${pageNo}`)
+    .then(res => {
+      if (!res.ok) {
+        throw res;
+      }
+      return res.json();
+    })
 
-  for (let guGun of seoulGuguns) {
-    const url = `${endPoint}?serviceKey=${serviceKey}&searchYearCd=${searchYearCd}&siDo=${siDo}&guGun=${guGun}&type=${type}&numOfRows=${numOfRows}&pageNo=${pageNo}`
-      promise = fetch(url)
-       .then(res => {
-         if (!res.ok) {
-           throw res;
-         }
-         return res.json();
-     })
-    console.log(url);
-  }  
-  
   return promise;
-  //total += response[0].acc_cnt
-  //kidtotal += response[1].acc_cnt
-  //oldtotal += response[2].acc_cnt
-
-
-
-
-
-
 }
 
-export default function App() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [data, setData] = useState(null);
-    const [searchYearCd, setSearchYearCd] = useState(2019);
-    //const [siDo, setSiDo] = useState(1100);
-    //const [guGun, setGuGun] = useState(null);
-    
 
-    
-    console.log(GUGUN);
-    
+const seoulGuguns = [
+  {name: "강남구", code: "1116"},
+  {name: "강동구", code: "1117"},
+  {name: "강북구", code: "1124"},
+  {name: "강서구", code: "1111"},
+  {name: "관악구", code: "1115"},
+  {name: "광진구", code: "1123"},
+  {name: "구로구", code: "1112"},
+  {name: "금천구", code: "1125"},
+  {name: "노원구", code: "1122"},
+  {name: "도봉구", code: "1107"},
+  {name: "동대문구", code: "1105"},
+  {name: "동작구", code: "1114"},
+  {name: "마포구", code: "1110"},
+  {name: "서대문구", code: "1109"},
+  {name: "서초구", code: "1119"},
+  {name: "성동구", code: "1104"},
+  {name: "성북구", code: "1106"},
+  {name: "송파구", code: "1118"},
+  {name: "양천구", code: "1120"},
+  {name: "영등포구", code: "1113"},
+  {name: "용산구", code: "1103"},
+  {name: "은평구", code: "1108"},
+  {name: "종로구", code: "1101"},
+  {name: "중구", code: "1102"},
+  {name: "중랑구", code: "1121"}
+]
+
+
+
+export default function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState(null);
+  const [searchYearCd, setSearchYearCd] = useState(2021);
+  const [guGun, setGuGun] = useState(1116);
+       
   useEffect(() => {  
     setIsLoaded(false);
 
-    fetchData(searchYearCd)
+    fetchData(searchYearCd, guGun)
     .then(data => {
       setData(data);
     })
     .catch(error => {
       setError(error)
     })
-    .finally(() => setIsLoaded(true))
-  }, [searchYearCd, siDo])
+    .finally(() => {
+      setIsLoaded(true)
+    })
+
+  }, [searchYearCd, guGun])
 
   if (error) {
     return <p>failed to fetch</p>
@@ -110,88 +100,201 @@ export default function App() {
     return <p>fetching data...</p>
   }
 
+  console.log(data)
+
+// 연도별, 대상별 사고건수
+  let allAcc = `${data.items.item[0].acc_cnt}`;
+  console.log(searchYearCd,data.items.item[0].sido_sgg_nm,"전체사고",allAcc);
+
+  let kidAcc = `${data.items.item[1].acc_cnt}`;
+  console.log(searchYearCd,data.items.item[0].sido_sgg_nm,"어린이사고",kidAcc)
+
+  let gugunName = `${data.items.item[0].sido_sgg_nm}`.substring(6)
+
+  console.log(data.items.item[1].tot_acc_cnt)
+
+  const handleSelect = (code) => {
+    setGuGun(code)
+  }
+ 
+
+  function open() {
+    const list = document.getElementById("list")
+    list.classList.toggle("hidden")
+  }
+
 
   return (
     <div style={{margin: "1rem"}}>
-      <h1>{searchYearCd}년 서울특별시 마포구 자전거 사고조회 &#128561;</h1>
+      <h2>{searchYearCd}년 서울특별시 {gugunName} 자전거 사고조회 &#128561;</h2>
 
-      <h2>조회하실 연도를 선택하십시오</h2>
+      {/* <h3>조회하실 연도를 선택하십시오</h3> */}
       <div className="">
         <button onClick={() => setSearchYearCd(searchYearCd - 1)}>&#10094; 이전년도</button>
-        <button onClick={() => setSearchYearCd(searchYearCd + 1)}>다음년도 &#10095;</button>
+        <button onClick={() => setSearchYearCd(searchYearCd + 1)} disabled={searchYearCd==2021}>다음년도 &#10095;</button>
+      </div>
+       
+
+        <>
+          <p id='pc-p' className='pfs'>{searchYearCd}년 전국에서 총 {data.items.item[0].tot_acc_cnt}건의 사고가 발생했습니다</p>
+
+        <div id='selector'>
+          <button
+            onClick={open}
+          >{gugunName}   &#9661;</button>
+          <div id='list' className='hidden'>
+            {seoulGuguns.map(gogun => (
+              <button
+              onClick={() => handleSelect(gogun.code)}
+              className="block hover:bg-yellow-600 p-2 w-full"
+              >
+                {gogun.name}
+              </button>
+            ))}
+          </div>
+        </div>
+       
+        <div id='pc-info' className='cm cm-bc border'>
+          <div className='chart cm-bc border ch-r'>
+            <h2 className='h2pl mb cm-bc td'>Chart</h2>
+            <div className='cm-bc'>
+              <h3>{gugunName} 총 사고건수: {allAcc}</h3>
+              <Rechart
+                allAcc={allAcc}
+                kidAcc={kidAcc}
+                gugunName={gugunName}
+                accidents={data.items}
+                searchYearCd={searchYearCd}
+              />
+            </div>
+          </div>
+
+         <div className='chart ch-l map'>
+          <h2 className='h2pl td'>지도</h2>
+            <p className='h2pl'>지도를 확대 또는 축소할 수 있습니다</p>
+            {/* <KakaoMap accidents={data.items.item} /> */}
+          </div>
+        </div>
+
+      <div id='mobile-info'>
+        <div className='chart mo-chart cm-bc border ch-r'>
+         <h2 className='h2pl mb cm-bc td'>Chart</h2>
+         <div className='cm-bc'>
+          <h3>{gugunName} 총 사고건수: {allAcc}</h3>
+          {/* <Rechart
+            gugunData={data}
+            gugunName={gugunName}/> */}
+          </div>
+
+          <div className='chart mo-map cm-bc'>
+          <h2 className='h2pl td cm-bc'>지도</h2>
+            <p className='h2pl cm-bc'>지도를 확대 또는 축소할 수 있습니다</p>
+            {/* <KakaoMap accidents={data.items.item} /> */}
+          </div>
+        </div>
       </div>
 
-      {data.totalCount > 0 ? (
-        <>
-          <h2>요약</h2>
-          <p>총 {data.totalCount}건의 사고가 발생했습니다</p>
-
-          <h2>Chart</h2>
-          <Rechart accidents={data.items.item} />
-
-          <h2>지도</h2>
-          <p>지도를 확대 또는 축소할 수 있습니다</p>
-        </>
-      ) : (
-        <p>해당 년도 자료가 없습니다</p>
-      )}
-
-      <li>{GUGUN}</li>
-    </div>  
+      </>
+    </div>
+       
   )
 }
 
-function Rechart({accidents}) {
+function fData(searchYearCd) {
+  const serviceKey = process.env.REACT_APP_SERVICE_KEY;
+  const promise = fetch(`http://apis.data.go.kr/B552061/lgStat/getRestLgStat?serviceKey=${serviceKey}&searchYearCd=${searchYearCd}&siDo=1100&guGun=&type=json&numOfRows=13&pageNo=1`)
+    .then(res => {
+      if (!res.ok) {
+        throw res;
+      }
+      return res.json();
+    })
+  return promise;
+}
 
-  const chartData = accidents.map(accident => {
-    return {
-      name: accident.spot_nm.split(' ')[2],
-      발생건수: accident.occrrnc_cnt,
-      중상자수: accident.se_dnv_cnt,
-      사망자수: accident.dth_dnv_cnt
-    }
-  })
+function Rechart(props) {
+  const [sidoData, setSidoData] = useState(null);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {  
+    fData(props.searchYearCd)
+    .then(data => {
+      setSidoData(data);
+    })
+    .catch(error => {
+      setError(error)
+    })
+  }, [])
+  if (error) {
+    return <p>failed to fetch</p>
+  }
+
+  console.log(sidoData.items.item[0].acc_cnt);
+
+
+  console.log(`${props.allAcc}`);
+ 
+  const data01 = [
+  { name: 'Group A', value:300},
+  { name: 'Group B', value: 300 },
+  ];
+
+
+  // const data02 = [
+  //   {name: "Group A", value: 400},
+  //   {name: "전국대비 강남구 어린이사고", value: `${props.kidAcc/props.accidents.item[1].tot_acc_cnt}`,}
+    
+  // ];
+  // const data03 = [
+  //   {name: "a", value: 99}
+  //   {name: "강남구 사고 대비 어린이사고 비율", value: `${props.kidAcc/props.allAcc}`}
+  // ];
+
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
   return (
-    // <div style={{ height: "300px" }}>
-    //   <ResponsiveContainer width="100%" height="100%">
-    //     <BarChart
-    //       width={500}
-    //       height={300}
-    //       data={chartData}
-    //       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    //     >
-    //       <CartesianGrid strokeDasharray="3 3" />
-    //       <XAxis dataKey="name" />
-    //       <YAxis />
-    //       <Tooltip />
-    //       <Legend />
-    //       <Bar dataKey="발생건수" fill="#0af" />
-    //       <Bar dataKey="중상자수" fill="#fa0" />
-    //       <Bar dataKey="사망자수" fill="#f00" />
-    //     </BarChart>
-    //   </ResponsiveContainer>
-    // </div>
-
-      <ComposedChart
-        layout="vertical"
-        width={500}
-        height={600}
-        data={chartData}
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 20,
-          left: 20
-        }}
-      >
-      <CartesianGrid stroke="#00" />
-      <XAxis type="number" />
-      <YAxis dataKey="name" type="category" scale="band" />
-      <Tooltip />
-      <Legend />   
-      <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-      <Bar dataKey="uv" barSize={20} fill="#413ea0" />
-    </ComposedChart>
+    <div style={{ height: "500px" }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx={500}
+            cy={350}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          >
+            {data01.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx={150}
+            cy={350}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx={300}
+            cy={150}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
+  
 }
